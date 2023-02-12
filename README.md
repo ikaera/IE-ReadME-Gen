3464e7ae8d0502cbc54dfe850b55a27b174ed83d
ikaera <ikaera@gmail.com>
2023-02-12 11:29:23 -0500

    create questions array.

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+User Story
+
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
+
+## Acceptance Criteria
+
+GIVEN a command-line application that accepts user input
+
+- WHEN I am prompted for information about my application repository
+  - THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+- WHEN I enter my project title
+- THEN this is displayed as the title of the README
+- WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+  - THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+- WHEN I choose a license for my application from a list of options
+  - THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+- WHEN I enter my GitHub username
+  - THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+- WHEN I enter my email address
+  - THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+- WHEN I click on the links in the Table of Contents
+
+  - THEN I am taken to the corresponding section of the README
+
+  ## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- index.js
+++ index.js
@@ -1,34 +1,120 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require('./utils/generateMarkdown');
-const inquirer = require('inquirer')
-const fs = require('fs/promises')
+const inquirer = require('inquirer');
+const fs = require('fs/promises');
 
-    // 'What is the Project-Title?',
-    // 'What is Description[User Story]?',
-    // 'Table of Contents?',
-    // 'Installation?',
-    // 'What is the Usage[Acceptance Criteria] (including screensot and/or demo) and What Features were used?',    
-    // 'License?',
-    // 'Contirbuting?',
-    // 'Tests?',
-    // 'Questions?'
+// 'What is the Project-Title?',
+// 'What is Description[User Story]?',
+// 'Table of Contents?',
+// 'Installation?',
+// 'What is the Usage[Acceptance Criteria] (including screensot and/or demo) and What Features were used?',
+// 'License?',
+// 'Contirbuting?',
+// 'Tests?',
+// 'Questions?'
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: 'input',
     name: 'title',
-    message: 'What is the Project-Title?', 
+    message: 'What is the title of your project?',
+    // default: ,
+    validate: answer => {
+      if (answer === '') {
+        return 'Please enter a title.';
+      }
+      return true;
+    },
   },
-  // {
+  {
+    type: 'input',
+    name: 'description',
+    message:
+      'Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide: (1)What was your motivation? (2) Why did you build this project? (3)  What problem does it solve? (4) What did you learn?',
+    // default: ,
+    // validate: ,
+  },
+  {
+    type: 'checkbox',
+    message:
+      'If your README is long, add a table of contents to make it easy for users to find what they need.',
+    name: 'tableOfContents',
+    choices: ['[Installation]', '[Usage]', '[Credits]', '[License]'],
+  },
+  {
+    type: 'input',
+    name: '[Installation](#installation)',
+    message:
+      'What are the steps required to install the project? Provide a step-by-step description of how to get the development environment running, please.',
+    default: '',
+    // validate:
+  },
 
-  // },
-  // {
+  {
+    type: 'input',
+    name: 'usage',
+    message:
+      'Provide instructions and examples for use, please. (Include screenshots as needed).',
+    default: '',
+    // validate:
+  },
 
-  // },
-  // {
+  {
+    type: 'input',
+    name: 'cresits',
+    message:
+      'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.If you followed tutorials, include links to those here as well.',
+    default: '',
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Choose an open source license, please.',
+    choices: [
+      'Apache License 2.0',
+      'GNU General Public License v3.0',
+      'MIT License',
+      'BSD 2-Clause "Simplified" License,',
+      'BSD 3-Clause "New" or "Revised" License',
+      'Boost Software License 1.0',
+    ],
+    default:
+      'This lets other developers know what they can and cannot do with your project.',
+  },
 
-  // },
+  {
+    type: 'input',
+    name: 'features',
+    message: 'Please, enter the features of your project.',
+    // default:
+    // validate:
+  },
+
+  {
+    type: 'input',
+    name: 'Contributing',
+    message:
+      "If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.",
+    // default:
+    // validate:
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message:
+      'If you have written the tests for your application, please, provide examples on how to run them here.',
+  },
+  {
+    type: 'input',
+    name: 'GitHub',
+    message: 'Enter your GitHub username here, please.',
+  },
+  {
+    type: 'input',
+    name: 'emailAddress',
+    message: 'Enter your email address here, please.',
+  },
 ];
 
 inquirer
@@ -40,17 +126,19 @@ inquirer
     // Use user feedback for... whatever!!
     console.log(answers);
     const markdown = generateMarkdown(answers);
-    console.log(markdown)
-    return fs.writeFile('./output/Readme.md', markdown)
+    console.log(markdown);
+    const fileName = `${answers.title.toLowerCase().split(' ').join('')}.md`;
+
+    return fs.writeFile(`./output/${fileName}`, markdown);
   })
-  .catch((error) => {
-    console.error(error)
+  .catch(error => {
+    console.error(error);
     // if (error.isTryError) {
     //   // Prompt couldn't be rendered in the current environment
     // }else {
     //   // Something else went wrong
     // }
-  })
+  });
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {
